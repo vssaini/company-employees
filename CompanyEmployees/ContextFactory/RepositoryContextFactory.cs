c81f23d928d2d2c49398fa02d05a3cb380c113cb
Vikram <vs00saini@gmail.com>
2022-02-15 19:04:15 +0530

    Add Repositories and Entities base projects

---
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,22 @@
+using Repository;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+        .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+            b => b.MigrationsAssembly("CompanyEmployees"));
+
+        return new RepositoryContext(builder.Options);
+
+    }
+
+}
---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Entities.Models;
+
+namespace Repository;
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options):base (options)
+    {
+        
+    }
+
+    public DbSet<Company>? Companies { get; set; }
+    public DbSet<Employee>? Employees { get; set; }
+}
