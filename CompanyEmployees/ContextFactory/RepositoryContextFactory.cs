28029c2a8acc6e8aa6558b7efda2ed7874205f64
Vikram Singh Saini <vssaini.dev@gmail.com>
2022-02-23 19:05:44 +0530

    Add database creation migration

--- CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -2,18 +2,20 @@ using Repository;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore;
 
+namespace CompanyEmployees.ContextFactory;
+
 public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
 {
     public RepositoryContext CreateDbContext(string[] args)
     {
         var configuration = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
-        .AddJsonFile("appsettings.json")
-        .Build();
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
 
         var builder = new DbContextOptionsBuilder<RepositoryContext>()
-        .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
-            b => b.MigrationsAssembly("CompanyEmployees"));
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+                 b => b.MigrationsAssembly("CompanyEmployees"));
 
         return new RepositoryContext(builder.Options);
 
--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -2,11 +2,12 @@
 using Entities.Models;
 
 namespace Repository;
+
 public class RepositoryContext : DbContext
 {
-    public RepositoryContext(DbContextOptions options):base (options)
+    public RepositoryContext(DbContextOptions options) : base(options)
     {
-        
+
     }
 
     public DbSet<Company>? Companies { get; set; }
