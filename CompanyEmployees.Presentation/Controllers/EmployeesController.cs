c4f9e52e122ef41c00a35c077de9a0adc7cf284a
Vikram Singh Saini <vssaini.dev@gmail.com>
2022-03-10 21:04:37 +0530

    Update employee using PUT request

--- CompanyEmployees.Presentation/Controllers/EmployeesController.cs
+++ CompanyEmployees.Presentation/Controllers/EmployeesController.cs
@@ -48,5 +48,16 @@ namespace CompanyEmployees.Presentation.Controllers
 
             return NoContent();
         }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateEmployeeForCompany(Guid companyId, Guid id, [FromBody] EmployeeForUpdateDto employee)
+        {
+            if (employee is null)
+                return BadRequest("EmployeeForUpdateDto object is null");
+
+            _service.EmployeeService.UpdateEmployeeForCompany(companyId, id, employee, false, true);
+
+            return NoContent();
+        }
     }
 }
--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -15,6 +15,8 @@ namespace CompanyEmployees
 
             CreateMap<CompanyForCreationDto, Company>();
             CreateMap<EmployeeForCreationDto, Employee>();
+
+            CreateMap<EmployeeForUpdateDto, Employee>();
         }
     }
 }
